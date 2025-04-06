4b580e04d8dbd7fb452e09e26be1ac75528a3196
anirudhgv1805 <anirudhgv04@gmail.com>
2025-04-06 14:51:41 +0530

    added local network access

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("The api is working perfectly with typescript enabled");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is listening on port ${PORT}`);
 });
