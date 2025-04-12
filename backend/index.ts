d45f08fdcf7fb200f7059fbe6aea48163db0a468
anirudhgv1805 <anirudhgv04@gmail.com>
2025-04-12 16:21:02 +0530

    dependency update

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const cors=require('cors');
-const app=express();
-app.use(express.json());
-app.use(cors());
-
-app.listen(5000,()=>{
-    console.log('Server is running on port 5000');
-});
